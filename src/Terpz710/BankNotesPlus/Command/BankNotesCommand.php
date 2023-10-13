720597e86bf795581762104b30ec2967516d9f0c
LazerPlayzYT <42108413+LazerPlayzYT@users.noreply.github.com>
2023-10-13 10:19:52 -0700

    Add files via upload

---
+++ src/Terpz710/BankNotesPlus/Command/BankNotesCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Terpz710\BankNotesPlus\Command;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat as TF;
+use Terpz710\BankNotesPlus\Main;
+
+class BankNotesCommand extends Command {
+
+    private $plugin;
+
+    public function __construct(Main $plugin) {
+        parent::__construct("banknotesplus", "Convert in-game money into bank notes", "/banknotesplus {amount}");
+        $this->setPermission("banknotesplus.cmd");
+        $this->plugin = $plugin;
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args) {
+        if (!$this->testPermission($sender)) {
+            return true;
+        }
+
+        if ($sender instanceof Player) {
+            if (count($args) === 1 && is_numeric($args[0]) && $args[0] > 0) {
+                $amount = (float)$args[0];
+
+                if ($this->plugin->hasEnoughMoney($sender, $amount)) {
+                    
+                    $this->plugin->convertToBankNote($sender, $amount);
+                    $sender->sendMessage(TF::GREEN . "You have converted $" . $amount . " into a bank note.");
+                } else {
+                    $sender->sendMessage(TF::RED . "You don't have enough money to convert into a bank note.");
+                }
+            } else {
+                $sender->sendMessage(TF::RED . "Usage: /banknotesplus {amount}");
+            }
+        } else {
+            $sender->sendMessage(TF::RED . "This command can only be used by players.");
+        }
+        return true;
+    }
+}
---
+++ src/Terpz710/BankNotesPlus/Main.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Terpz710\BankNotesPlus\Main;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\item\VanillaItems;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat as TF;
+use Terpz710\BankNotesPlus\Command\BankNotesCommand;
+use Terpz710\libEco\libEco;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getServer()->getCommandMap()->register("banknotesplus", new BankNotesCommand($this));
+    }
+
+    public function onPlayerInteract(PlayerInteractEvent $event): void {
+        $player = $event->getPlayer();
+        $item = $event->getItem();
+
+        if ($item->getNamedTag()->getTag("Amount") !== null) {
+            $amount = $item->getNamedTag()->getFloat("Amount");
+
+            $item->setCount($item->getCount() - 1);
+            $player->getInventory()->setItemInHand($item);
+
+            if (libEco::isInstall()) {
+                libEco::addMoney($player, $amount);
+                $player->sendMessage(TF::GREEN . "You have claimed $" . $amount . " from the bank note.");
+            } else {
+                $player->sendMessage(TF::RED . "libEco is not available. Unable to add money to your balance.");
+            }
+        }
+    }
+
+    public function hasEnoughMoney(Player $player, float $amount): bool {
+        $balance = 0;
+
+        if (libEco::isInstall()) {
+            libEco::myMoney($player, function(float $money) use (&$balance) {
+                $balance = $money;
+            });
+        }
+
+        return $balance >= $amount;
+    }
+
+    public function convertToBankNote(Player $player, float $amount): void {
+        $bankNote = VanillaItems::PAPER();
+        $bankNote->setCustomName(TF::GOLD . "$" . $amount . " Bank Note");
+        $bankNote->setLore([
+            "Value: $" . $amount,
+            "Right-click to redeem"
+        ]);
+        $bankNote->getNamedTag()->setFloat("Amount", $amount);
+
+        $player->getInventory()->addItem($bankNote);
+    }
+}
